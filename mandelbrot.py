645d8a31225b8191fa77af1dd73967a0d0e53c1f
James DeVries <jamesrdevries@gmail.com>
2024-07-28 10:43:17 -0500

    Fill array sequentially

--- mandelbrot.py
+++ mandelbrot.py
@@ -7,7 +7,11 @@ from pygame import surfarray
 import numpy as np
 from numpy import int32, uint
 
-def surfdemo_show(array_img):
+"Settings"
+screen_x = 512
+screen_y = 256
+
+def show_array(array_img):
     screen = pg.display.set_mode(array_img.shape[:2], 0, 32)
     surfarray.blit_array(screen, array_img)
     pg.display.flip()
@@ -22,18 +26,22 @@ def wait_click():
             pg.quit()
             raise SystemExit()
 
+def calculate_point(x, y):
+    r = x%256
+    g = y%256
+    b = (x*y)%256
+    return (r,g,b)
 
 def main():
     pg.init()
     pg.display.set_caption("mandelbrot")
 
-    # striped
-    # the element type is required for np.zeros in numpy else
-    # an array of float is returned.
-    striped = np.zeros((512, 256, 3), int32)
-    striped[:] = (255, 0, 0)
-    striped[:, ::3] = (0, 255, 255)
-    surfdemo_show(striped)
+    values = np.zeros((screen_x, screen_y, 3), int32)
+    for x in range(screen_x):
+        for y in range(screen_y):
+            (r,g,b) = calculate_point(x,y)
+            values[x][y] = (r,g,b)
+    show_array(values)
 
     wait_click()
 
