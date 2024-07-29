6e49ccaa56210a49c0db7506cdde0548541f1dc1
James DeVries <jamesrdevries@gmail.com>
2024-07-28 19:55:56 -0500

    Fancier colors

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import time
 
 import pygame as pg
 from pygame import surfarray
@@ -10,7 +11,7 @@ from numpy import int32, uint
 "Settings"
 screen_x = 1024
 screen_y = 768
-max_iter = 100
+max_iter = 150
 top_left = -1.5-1.25j
 bottom_right = 0.75+1.25j
 
@@ -23,8 +24,8 @@ color_black = (0,0,0)
 color_red = (255,0,0)
 color_green = (0,255,0)
 color_blue = (0,0,255)
-c_step1 = max_iter//3
-c_step2 = c_step1 * 2
+c_step1 = max_iter//8
+c_step2 = c_step1 * 3
 
 
 def show_array(array_img):
@@ -35,6 +36,7 @@ def show_array(array_img):
 def wait_click():
     while True:
         e = pg.event.wait()
+        
         # Force application to only advance when main button is released
         if e.type == pg.MOUSEBUTTONUP and e.button == pg.BUTTON_LEFT:
             break
@@ -63,9 +65,9 @@ def calculate_point(x, y):
 
 def color_point(i):
     if i < c_step1:
-        return color_red        
+        return tuple(z * (i/c_step1) for z in color_red)
     if i < c_step2:
-        return color_green
+        return tuple(z * (i/c_step2) for z in color_green)
     elif i < max_iter:
         return color_blue
     return color_black
@@ -75,10 +77,14 @@ def main():
     pg.display.set_caption("mandelbrot")
 
     values = np.zeros((screen_x, screen_y, 3), int32)
+    time_s = time.time()
     for x in range(screen_x):
         for y in range(screen_y):
             (r,g,b) = calculate_point(x,y)
             values[x][y] = (r,g,b)
+
+    time_e = time.time()
+    print(time_e - time_s)
     show_array(values)
 
     wait_click()
